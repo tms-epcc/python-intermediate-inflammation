e3e963240ed6b002d9477842fcd4bf6174265f64
tms-epcc <t.sloan@epcc.ed.ac.uk>
2021-12-07 16:13:49 +0000

    Add patient_normalise function and related tests

--- inflammation/models.py
+++ inflammation/models.py
@@ -44,6 +44,21 @@ def daily_min(data):
     """
     return np.min(data, axis=0)
 
+def patient_normalise(data):
+    """
+    Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values are rounded to 0.
+    """
+    max = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
+
 
 # TODO(lesson-design) Add Patient class
 # TODO(lesson-design) Implement data persistence
--- tests/test_models.py
+++ tests/test_models.py
@@ -4,6 +4,7 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
+
 @pytest.mark.parametrize(
     "test, expected",
     [
@@ -14,6 +15,7 @@ def test_daily_mean(test, expected):
     """Test mean function works for array of zeroes and positive integers."""
     from inflammation.models import daily_mean
     npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -55,6 +57,7 @@ def test_daily_max(test, expected):
     from inflammation.models import daily_max
     npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
+
 def test_daily_max_integers():
     # was original test_daily_max
     """Test that max function works for an array of positive integers."""
@@ -66,6 +69,7 @@ def test_daily_max_integers():
     test_result = np.array([4, 6, 9])
 
     npt.assert_array_equal(daily_max(test_input), test_result)
+
 
 @pytest.mark.parametrize(
     "test, expected",
@@ -79,17 +83,19 @@ def test_daily_min(test, expected):
     from inflammation.models import daily_min
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
+
 def test_daily_min_integers():
     # was original test_daily_min
     """Test that min function works for an array of positive and negative integers."""
     from inflammation.models import daily_min
 
-    test_input = np.array([[ 4, -2, 5],
-                           [ 1, -6, 2],
+    test_input = np.array([[4, -2, 5],
+                           [1, -6, 2],
                            [-4, -1, 9]])
     test_result = np.array([-4, -6, 2])
 
     npt.assert_array_equal(daily_min(test_input), test_result)
+
 
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
@@ -97,3 +103,41 @@ def test_daily_min_string():
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        (
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        ),
+        (
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+        ),
+        (
+           [[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]],
+           [[0, 1, 1], [1, 1, 1], [1, 1, 1]]
+        ),
+        (
+           [[1, 2, 3], [4, 5, float('nan')], [7, 8, 9]],
+           [[0.33, 0.67, 1], [0.8, 1, 0], [0.78, 0.89, 1]]
+       ),
+       (
+           [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+           [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]
+        )
+    ]
+)
+def test_patient_normalise(test, expected):
+    """
+       Test normalisation works for arrays of one and positive integers.
+       Assumption that test accuracy of two decimal places is sufficient.
+    """
+    from inflammation.models import patient_normalise
+    npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
